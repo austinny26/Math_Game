daacc08d97f380bde9c9a6ec04dd12774f44751c
adsda <fd@gmail.com>
2025-10-29 13:59:45 -0400

    Starting screen implemented

--- app/src/main/java/com/example/mathgame/MainActivity.kt
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.mathgame
 
-import android.R
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,13 +26,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.mathgame.ui.theme.MathGameTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +47,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 fun MainLayout() {
 
+    var isPlaying by remember { mutableStateOf(false) }
     var backColor by remember { mutableStateOf(Color.Yellow) }
     Surface(
         color = backColor,
@@ -54,14 +60,34 @@ fun MainLayout() {
             modifier = Modifier
                 .fillMaxSize(),
             ) {
-            StartScreen()
+            if(isPlaying){
+                PlayScreen()
+            }else {
+                StartScreen(onStartClicked = {isPlaying = true})
+            }
         }
 
     }
 }
 
+
 @Composable
-fun StartScreen() {
+fun PlayScreen(){
+    Surface(
+        color = Color.Red
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            )
+        {
+
+        }
+    }
+
+}
+@Composable
+fun StartScreen(onStartClicked: () -> Unit) {
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -69,7 +95,7 @@ fun StartScreen() {
     ) {
         Button(
             onClick = {
-
+                onStartClicked
             }
         ) {
             Text(
@@ -77,15 +103,29 @@ fun StartScreen() {
                 fontSize = 24.sp
             )
         }
+
+        Spacer(modifier = Modifier.size(32.dp))
+
         Text(text = "Tap start to begin!", fontSize = 32.sp)
 
-        Surface(
+        Spacer(modifier = Modifier.size(32.dp))
 
-        ) {
-            Image(
-                painter = painterResource(R.drawable.launchimage),
-                contentDescription = "start"
-            )
-        }
+            Surface (
+                modifier = Modifier
+                    .size(width = 300.dp, height = 200.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(BorderStroke(4.dp, Color.Red)),
+
+            ){
+                Image(
+                    painter = painterResource(R.drawable.launchimage),
+                    contentDescription = "start",
+                    modifier = Modifier
+                        .fillMaxSize()
+
+
+                )
+            }
+
     }
 }
