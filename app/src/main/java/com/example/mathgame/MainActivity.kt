ae9dd5873856416ee1ee3c1dd81787d91b16893c
adsda <fd@gmail.com>
2025-10-31 13:59:30 -0400

    added boxes with random ints but greater number logic not added

--- app/src/main/java/com/example/mathgame/MainActivity.kt
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.mathgame
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -30,12 +28,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.nio.file.WatchEvent
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -129,16 +126,50 @@ fun PlayScreen(
             Text("Tap the Larger Number!", fontSize = 32.sp)
             Spacer(modifier = Modifier.size(48.dp))
 
-            Button(
-                onClick = {
+            var greater by remember { mutableStateOf(Random.nextInt(1,100))}
+            var lesser by remember { mutableStateOf(Random.nextInt(1,100))}
+            var temp by remember { mutableStateOf(0) }
 
-                }
-            ) {
-                Text("Back to Start")
+            if (greater <= lesser){
+                temp = greater
+                greater = lesser
+                lesser = temp
             }
+            NumBox(Color(0xFF90D3F1), greater, lesser)
+            Spacer(modifier = Modifier.size(48.dp))
+            NumBox(Color(0xFFFFB342), lesser, lesser)
+
         }
     }
 }
+
+@Composable
+fun NumBox(color: Color, random:Int, lesser: Int) {
+
+    Surface(
+        modifier = Modifier.size(150.dp)
+            .clickable(
+                onClick = {
+                    if(random < lesser){
+
+                    }
+                }
+            ),
+        color = color,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+         Text(
+               text = "$random",
+               fontSize = 64.sp
+         )
+        }
+    }
+}
+
 @Composable
 fun GameOverScreen(onRestart: () -> Unit) {
 
